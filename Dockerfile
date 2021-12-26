8291c44ba665602cfad528c35002f77d3ce75fae
Badrri Narayanan S <badrri1995@gmail.com>
2021-12-26 19:09:59 +0530

    added docker to project

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:16-bullseye-slim
+
+WORKDIR /usr/src/face-detector-api
+
+CMD ["/bin/bash"]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan')
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -21,14 +22,17 @@ const db = knex({
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('combined'));
 
-app.get('/', (req, res) => {res.send('The server is running')});
+app.get('/', (req, res) => {res.send('The server is running and ready')});
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt)});
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApi(req, res, db)});
 
-app.listen(process.env.PORT, () => {
-	console.log(`Server is online and is running on PORT ${process.env.PORT}`);
+let port = process.env.PORT || 3004
+
+app.listen(port, () => {
+	console.log(`Server is online and is running on PORT ${port}`);
 });

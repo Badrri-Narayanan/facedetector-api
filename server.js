91bda8e2d579c21f7b5a7a63f60199453fcb271a
Badrri Narayanan S <badrri1995@gmail.com>
2022-01-04 23:06:25 +0530

    made corrections to update profile endpoint

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => res.send('The server is up and running.'));
 app.post('/signin', signin.signInAuthentication(db, bcrypt));
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', auth.requireAuth, (req, res) => profile.handleProfileGet(req, res, db));
-app.post('/profile/:id', auth.requireAuth, (req, res) => profile.handleProfileUpdate(req, res, db));
+app.put('/profile/:id', auth.requireAuth, (req, res) => profile.handleProfileUpdate(req, res, db));
 app.put('/image', auth.requireAuth, (req, res) => image.handleImage(req, res, db));
 app.post('/imageurl', auth.requireAuth, (req, res) => image.handleApi(req, res, db));
 
